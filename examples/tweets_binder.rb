cc5391c0b2ddeddb7d260b36df2abbb5c3e79e4e
Alex MacCaw <maccman@gmail.com>
2009-12-22 18:20:39 +0000

    Update examples

--- examples/tweets_binder.rb
+++ examples/tweets_binder.rb
@@ -1,8 +1,6 @@
 class TweetsBinder < Bowline::Binders::Base
   bind Tweet
-  class << self
-    def update(status)
-      klass.update(status)
-    end
+  def self.update(status)
+    klass.update(status)
   end
 end
