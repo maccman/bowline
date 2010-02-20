be2013559806a7bd72d70609419e25ab1939103f
Alex MacCaw <maccman@gmail.com>
2010-02-20 15:26:21 +0000

    Add Network module for offline/online events

--- lib/bowline/desktop/network.rb
+++ lib/bowline/desktop/network.rb
@@ -1,7 +1,67 @@
+require "socket"
+require "timeout"
+
 module Bowline
   module Desktop
-    class Network #:nodoc:
-      # Implement address polling to see if we're online
+    module Network
+      extend Bowline::Watcher::Base
+      watch :on_online, :on_offline, :on_change
+      
+      def online!
+        return if @online
+        @online = true
+        watcher.call(:on_online)
+        watcher.call(:on_change)
+      end
+      
+      def offline!
+        return unless @online
+        @online = false
+        watcher.call(:on_offline)
+        watcher.call(:on_change)
+      end
+      
+      def online?
+        @online
+      end
+      
+      def host
+        @host ||= "google.com"
+      end
+      
+      def host=(host)
+        @host = host
+      end
+      
+      def port
+        @port ||= 80
+      end
+      
+      def port=(port)
+        @port = port
+      end
+      
+      def poll!
+        Thread.new do
+          loop do
+            ping ? online! : offline!
+            sleep 30
+          end
+        end
+      end
+            
+      private
+            
+        def ping
+          Timeout::timeout(3) {
+            TCPSocket.new(host, port).close
+          }
+          true
+        rescue Errno::ECONNREFUSED, Timeout::Error, StandardError
+          false
+        end
+      
+      extend self
     end
   end
 end
